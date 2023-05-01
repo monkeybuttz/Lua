f3f913cede74bd06e85ecb86c1a7c4ef09c45219
monkeybuttz <g2yonan@gmail.com>
2023-05-01 10:51:50 -0500

    final

--- dataTable.lua
+++ dataTable.lua
@@ -1,41 +1,54 @@
 -- read csv file and store in a table
+
 -- Function to split a string by a separator
 function split(str, sep)
     local result = {}
-    local regex = "([^" .. sep .. "]+)"
-    for part in string.gmatch(str, regex) do
-        table.insert(result, part)
+    local regex = ("([^%s]+)"):format(sep)
+    for each in str:gmatch(regex) do
+        table.insert(result, each)
     end
     return result
 end
 
 -- Function to read CSV file and return a table
 function read_csv(file_path)
-    local data = {}
-    local headers
-    for line in io.lines(file_path) do
-        if not headers then
-            headers = split(line, "\t")
-        else
-            local row = {}
-            local values = split(line, "\t")
-            for i, value in ipairs(values) do
-                row[headers[i]] = value
+    local file = io.open(file_path, "r")
+    local table_data = {}
+    local headers = {}
+    local row_count = 0
+    for line in file:lines() do
+        local row = split(line, ",")
+        if row_count == 0 then
+            for i, col in ipairs(row) do
+                headers[i] = col
             end
-            table.insert(data, row)
+        else
+            table_data[row_count] = row
         end
+        row_count = row_count + 1
     end
-    return data, headers
+    file:close()
+    return table_data, headers
 end
 
 -- Read CSV file and store the data in a table
 local file_path = "data.csv"
 local table_data, headers = read_csv(file_path)
 
--- Print the table
-for _, row in ipairs(table_data) do
-    for _, header in ipairs(headers) do
-        io.write(header .. ": " .. row[header] .. "\t")
+-- Print out the table to prove data is stored correctly
+for i, header in ipairs(headers) do
+    io.write(header)
+    if i < #headers then
+        io.write(",    ")
+    end
+end
+io.write("\n")
+for i, row in ipairs(table_data) do
+    for j, col in ipairs(row) do
+        io.write(col)
+        if j < #row then
+            io.write(", ")
+        end
     end
     io.write("\n")
 end
